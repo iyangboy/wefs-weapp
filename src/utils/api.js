a304337e3f9976f1c99f9935d2ae5a29d86d3dae
杨万里 <1091231787@qq.com>
2019-09-19 14:43:05 +0800

    switch env

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,7 +1,8 @@
 import wepy from 'wepy'
 
 // 服务器接口地址
-const host = 'http://wefs.test/api'
+// const host = 'http://wefs.test/api'
+const host = __BASE_URL__
 
 // 普通请求
 const request = async (options, showLoading = true) => {
